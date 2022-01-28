e3875bd222b5fc02d3ccc7e3ff06ab5b4ff6fbd6
grouazel <antoine.grouazel@ifremer.fr>
2022-01-28 12:49:10 +0100

    add a mode monthly for get KPI values each month for the last 12 months

--- src/kpi_WV_hs/compute_kpi_1d_v2_prun.py
+++ src/kpi_WV_hs/compute_kpi_1d_v2_prun.py
@@ -6,6 +6,7 @@ import sys
 print(sys.executable)
 import subprocess
 import logging
+import calendar
 from dateutil import rrule
 import datetime
 import getpass
@@ -17,6 +18,8 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser (description='start prun')
     parser.add_argument ('--verbose',action='store_true',default=False)
+    parser.add_argument('--mode', choices=['range_dates','monthly'], default='range_dates',
+                        help='mode : range_dates (last 20 days) or monthly (last 12 months)')
     args = parser.parse_args ()
     if args.verbose:
         logging.basicConfig (level=logging.DEBUG,format='%(asctime)s %(levelname)-5s %(message)s',
@@ -34,24 +37,41 @@ if __name__ == '__main__':
     # call prun
     opts = ' --split-max-lines=3 --background -e '
     listing_content = []
-    sta = datetime.datetime(2015,1,1)
-    #sta = datetime.datetime(2019,6,1)
+    if args.mode == 'range_dates':
+        #sta = datetime.datetime(2015,1,1)
+        #sta = datetime.datetime(2019,6,1)
 
-    #sta = datetime.datetime(2020,6,1) # pour test 2 qui utilisent les cross assignments de partitions
+        #sta = datetime.datetime(2020,6,1) # pour test 2 qui utilisent les cross assignments de partitions
 
-    sto = datetime.datetime.today()
-    sta = sto - datetime.timedelta(days=20)
-    logging.info('start year: %s', sta)
-    #sto = datetime.datetime(2020,9,1)
-    fid = open(listing,'w')
-    cpt = 0
-    for unit in ['S1A','S1B']:
-        for wv in ['wv1','wv2']:
-            logging.info('%s',unit)
-            for dd in rrule.rrule(rrule.DAILY,dtstart=sta,until=sto):
-                fid.write('%s %s %s\n'%(unit,wv,dd.strftime('%Y%m%d')))
-                cpt +=1
-    fid.close()
+        sto = datetime.datetime.today()
+        sta = sto - datetime.timedelta(days=20)
+        logging.info('start year: %s', sta)
+        #sto = datetime.datetime(2020,9,1)
+        fid = open(listing,'w')
+        cpt = 0
+        for unit in ['S1A','S1B']:
+            for wv in ['wv1','wv2']:
+                logging.info('%s',unit)
+                for dd in rrule.rrule(rrule.DAILY,dtstart=sta,until=sto):
+                    fid.write('%s %s %s\n'%(unit,wv,dd.strftime('%Y%m%d')))
+                    cpt +=1
+        fid.close()
+    elif args.mode=='monthly': # last day of each month is used to compute the KPI on the last 12 months
+        fid = open(listing, 'w')
+        cpt = 0
+        sto = datetime.datetime.today()
+        sta = sto - datetime.timedelta(days=366)
+        for unit in ['S1A', 'S1B']:
+            for wv in ['wv1', 'wv2']:
+                logging.info('%s', unit)
+                for dd in rrule.rrule(rrule.MONTHLY, dtstart=sta, until=sto):
+                    start, stop = calendar.monthrange(dd.year, dd.month)
+                    dd2 = dd.replace(day=stop)
+                    fid.write('%s %s %s\n' % (unit, wv, dd2.strftime('%Y%m%d')))
+                    cpt += 1
+        fid.close()
+    else:
+        raise Exception("mode unknown")
     logging.info('listing written ; %s nb lines: %s',listing,cpt)
 
     cmd = prunexe+opts+pbs+' '+listing
--- src/kpi_WV_nrcs/compute_kpi_1b_prun.py
+++ src/kpi_WV_nrcs/compute_kpi_1b_prun.py
@@ -7,6 +7,7 @@ print(sys.executable)
 import subprocess
 import logging
 import getpass
+import calendar
 from dateutil import rrule
 import datetime
 if __name__ == '__main__':
@@ -17,6 +18,8 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser (description='start prun')
     parser.add_argument ('--verbose',action='store_true',default=False)
+    parser.add_argument('--mode', choices=['range_dates','monthly'], default='range_dates',
+                        help='mode : range_dates (last 20 days) or monthly (last 12 months)')
     args = parser.parse_args ()
     if args.verbose:
         logging.basicConfig (level=logging.DEBUG,format='%(asctime)s %(levelname)-5s %(message)s',
@@ -38,19 +41,39 @@ if __name__ == '__main__':
     #sta = datetime.datetime(2019,6,1)
 
     #sta = datetime.datetime(2020,6,1) # pour test 2 qui utilisent les cross assignments de partitions
-    logging.info('start year: %s',sta)
-    sto = datetime.datetime.today()
-    sta = sto - datetime.timedelta(days=20)
-    #sto = datetime.datetime(2020,9,1)
-    fid = open(listing,'w')
-    cpt = 0
-    for unit in ['S1A','S1B']:
-        for wv in ['wv1','wv2']:
-            logging.info('%s',unit)
-            for dd in rrule.rrule(rrule.DAILY,dtstart=sta,until=sto):
-                fid.write('%s %s %s\n'%(unit,wv,dd.strftime('%Y%m%d')))
-                cpt +=1
-    fid.close()
+    if args.mode=='range_dates':
+        logging.info('start year: %s',sta)
+        sto = datetime.datetime.today()
+        sta = sto - datetime.timedelta(days=20)
+        #sto = datetime.datetime(2020,9,1)
+        fid = open(listing,'w')
+        cpt = 0
+        for unit in ['S1A','S1B']:
+            for wv in ['wv1','wv2']:
+                logging.info('%s',unit)
+                for dd in rrule.rrule(rrule.DAILY,dtstart=sta,until=sto):
+                    fid.write('%s %s %s\n'%(unit,wv,dd.strftime('%Y%m%d')))
+                    cpt +=1
+        fid.close()
+    elif args.mode=='monthly': # last day of each month is used to compute the KPI on the last 12 months
+        sto = datetime.datetime.today()
+        sta = sto - datetime.timedelta(days=366)
+        # sto = datetime.datetime(2020,9,1)
+        fid = open(listing, 'w')
+        cpt = 0
+        for unit in ['S1A', 'S1B']:
+            for wv in ['wv1', 'wv2']:
+                logging.info('%s', unit)
+                for dd in rrule.rrule(rrule.MONTHLY, dtstart=sta, until=sto):
+                    start,stop = calendar.monthrange(dd.year,dd.month)
+                    dd2 = dd.replace(day=stop)
+                    fid.write('%s %s %s\n' % (unit, wv, dd2.strftime('%Y%m%d')))
+                    cpt += 1
+        fid.close()
+    else:
+        raise Exception("mode unknown")
+
+
     logging.info('listing written ; %s nb lines: %s',listing,cpt)
 
     cmd = prunexe+opts+pbs+' '+listing
