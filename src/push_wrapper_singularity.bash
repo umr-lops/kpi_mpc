3e9eba6e6041e9655da8d8f3105ef0d19de2b81c
grouazel <antoine.grouazel@ifremer.fr>
2025-03-26 14:16:35 +0000

    improve the singularity wrapper so that it can run with satwave on dgx nodes

--- src/push_wrapper_singularity.bash
+++ src/push_wrapper_singularity.bash
@@ -1,21 +1,31 @@
 #!/bin/bash
+#PBS -l walltime=01:30:00
+#PBS -l mem=1gb
+#PBS -m n
+#PBS -N push-inp-KPI-2-ECE
+#PBS -q ftp
 set -e
 # A Grouazel
 # Oct 2022
 echo start
-kpiname=$1
-datesta=$2
-datesto=$3
-
+#kpiname=$1
+#datesta=$2
+#datesto=$3
+echo "kpiname = $kpiname"
+echo "datesta = $datesta"
+echo "datesto = $datesto"
 if [ -z "$datesta" ]
 then
   datesta=`date -d ' -5 day' +%Y%m%d`
   datesto=`date -d ' -0 day' +%Y%m%d`
 fi
 
-
+ipadress=`ip addr | grep 134`
+echo "IP adress host $ipadress"
 echo $datesta $datesto
-#module load singularity/3.6.4
+source /usr/share/Modules/3.2.10/init/bash
+module load singularity/3.6.4
+echo 'singularity module loaded'
 
 pybin=/home/datawork-cersat-public/project/mpc-sentinel1/workspace/mamba/envs/xsar_oct22/bin/python
 
@@ -34,8 +44,10 @@ echo 'sing_image'$sing_image
 #-B /home3/homedir7 # not possible on datarmor nodes
 
 hostna=`hostname`
-if [ "${hostna}" == "datarmor" ] || [ "${hostna}" == "datarmor1" ] || [ "${hostna}" == "datarmor2" ] || [ "${hostna}" == "datarmor3" ] ; then
+#if [ "${hostna}" == "datarmor" ] || [ "${hostna}" == "datarmor1" ] || [ "${hostna}" == "datarmor2" ] || [ "${hostna}" == "datarmor3" ] ; then
+if [[ "${hostna}" =~ ^(datarmor[1-3]?|compute-101-(9|15|23))$ ]] ; then
   echo 'hostname is datarmor'
+  module load singularity/3.6.4
   singubin=/appli/singularity/3.6.4/bin/singularity
 else
   echo 'shotname is '$hostna
@@ -43,5 +55,6 @@ else
 fi
 #$singubin version
 echo 'ok for singularity call'
-$singubin exec -B /home1/datahome -B /home/datawork-cersat-public -B /home1/datawork -B /home1/scratch  $sing_image $pybin $exepy --kpi $kpiname --startdate $datesta --stopdate $datesto
+# previously singularity exec
+$singubin exec --bind /home1/datahome --bind /home/datawork-cersat-public --bind /home1/datawork --bind /home1/scratch  $sing_image $pybin $exepy --kpi $kpiname --startdate $datesta --stopdate $datesto
 echo done
